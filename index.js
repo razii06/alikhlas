ff227010a2ef868e9e8a5e6c5d550c9c7d30c384
Fakrul Razi <160989375+razii06@users.noreply.github.com>
2024-11-13 09:00:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-<<<<<<< HEAD
+// server/index.js
 import express from "express";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
-dotenv.config();
+dotenv.config(); // Pastikan dotenv di-load untuk mengakses variabel environment
 
-import { db } from "./configs/db.js";
+import { db } from "./configs/db.js";  // Mengimpor koneksi DB dari file db.js
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // Port default jika tidak ada di .env
 
-//middleware untuk parsing JSON pada request body
+// Middleware untuk parsing JSON pada request body
 app.use(express.json());
 app.use('/api/v1/users', userRouter);
 
+// Route untuk mengakses data
 app.get("/", (req, res) => {
     res.status(200).json({
-        message: "Hello, world!"
-    })
+        message: "Hello pak Reja!😊",
+    });
 });
 
 app.get("/data", async (req, res) => {
@@ -29,57 +31,16 @@ app.get("/data", async (req, res) => {
         res.status(500).json({ message: "Error fetching data", error });
     }
 });
-app.get("*", (req, res) => {
+
+
+// Route untuk menangani request yang tidak ditemukan
+app.use("*", (req, res) => {
     res.status(404).json({
-        message: "not found!"
-    })
+        message: "Not Found",
+    });
 });
 
-app.listen(PORT, () => { 
-    console.log(`server listening on port ${PORT}`)
+// Menjalankan server Express
+app.listen(PORT, () => {
+    console.log(`Server started, Listening on port ${PORT}`);
 });
-=======
-// Definisi Library yang digunakan
-const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const path = require('path');
-const flash = require('req-flash');
-const app = express();
-
-// Definisi lokasi file router
-const loginRoutes = require('./src/routes/router-login');
-const registerRoutes = require('./src/routes/router-register');
-const appRoutes = require('./src/routes/router-app');
-
-// Configurasi dan gunakan library
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-// Configurasi library session
-app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: 't@1k0ch3ng',
-    name: 'secretName',
-    cookie: {
-        sameSite: true,
-        maxAge: 60000
-    },
-}))
-app.use(flash());
-
-// Setting folder views
-app.set('views',path.join(__dirname,'src/views'));
-app.set('view engine', 'ejs');
-
-// Gunakan routes yang telah didefinisikan
-app.use('/login', loginRoutes);
-app.use('/register', registerRoutes);
-app.use('/', appRoutes);
-
-// Gunakan port server
-app.listen(5050, ()=>{
-    console.log('Server Berjalan di Port : '+5050);
-});
->>>>>>> 6e56227c72837991bbece3b7776decf6b2d55564
